863cb7a7a680c2786aaa4b66fce2568e1c96e0f0
Josh <jsh.a.powers@gmail.com>
2015-03-30 19:10:00 -0500

    cleaned up routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,26 @@ Router.route('/', function(){
     this.render('home');
 });
 
+Router.route('/home', function(){
+    this.render('home');
+});
+
 Router.route('/projects', function(){
     this.render('projects');
 });
 
 Router.route('/customers', function(){
     this.render('customersMain');
+});
+
+Router.route('/login', function(){
+    this.render('login');
+});
+
+Router.route('/signup', function(){
+    this.render('signup');
+});
+
+Router.route('/password', function(){
+    this.render('passwordRecovery');
 });
